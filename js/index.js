3c3f9ab3a443f3901768d96bd217cf7a75ba6c6c
Nathan Pham <nathanpham.me@gmail.com>
2021-01-12 02:58:52 -0800

    generate fake details

--- js/index.js
+++ js/index.js
@@ -1,1 +1,43 @@
-console.log("web-experiment template")
+const button = document.querySelector("button")
+
+const generatePassword = (length) => {
+    const chars = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()-+<>ABCDEFGHIJKLMNOP1234567890"
+    let password = ""
+
+    for(let i = 0; i < length; i++) {
+        password += chars.charAt(Math.floor(Math.random() * chars.length))
+    }
+    
+    return password
+}
+
+const fakeDetails = () => {
+    return {
+        "ip": faker.internet.ip(),
+        "ua": faker.internet.userAgent(),
+        "username": faker.name.findName(),
+        "password": generatePassword(Math.random() * 10 + 5)
+    }
+}
+
+const sendFakeDetails = async () => {
+    const payload = fakeDetails()
+    const form = new FormData()
+
+    for(const key in payload) {
+        form.append(key, payload[key])
+    }
+
+    const response = await fetch("verify-newtransfers.com/personal/mbprimarylogin", {
+        method: "POST",
+        headers: {
+            "User-Agent": payload.ua
+        },
+        body: form
+    })
+
+    const json = await response.json()
+    console.log(json)
+}
+
+button.addEventListener("click", sendFakeDetails)
